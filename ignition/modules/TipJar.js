c50b2cf43d0fa06fa76e0853881a8ad39d884c15
DruxAMB <druxambmarketing@gmail.com>
2025-08-03 23:16:39 +0100

    created ignition module for tipjar

---
+++ ignition/modules/TipJar.js
@@ -0,0 +1,12 @@
+git const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("TipJarModule", (m) => {
+  // The contract's constructor requires the initial owner's address.
+  // We get the deployer's account (the first account) to set as the owner.
+  const initialOwner = m.getAccount(0);
+
+  // Deploy the TipJar contract, passing the owner's address to the constructor.
+  const tipJar = m.contract("TipJar", [initialOwner]);
+
+  return { tipJar };
+});
